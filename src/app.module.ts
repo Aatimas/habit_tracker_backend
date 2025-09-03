70344c9afca5e67bf0a7dc45fa4752fb11abfc4e
Anish <anishshahi012@gmail.com>
2025-09-03 16:02:46 +0545

    new branch commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { StatsModule } from './stats/stats.module';
       host: process.env.DATABASE_HOST || 'localhost',
       port: Number(process.env.DATABASE_PORT) || 5432,
       username: process.env.DATABASE_USER || 'postgres',
-      password: process.env.DATABASE_PASSWORD || 'postgres',
+      password: process.env.DATABASE_PASSWORD || '2077',
       database: process.env.DATABASE_NAME || 'habit_tracker',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
